f93fd3fd6727d69155aafcfdbb9993302f35144f
cazhuo <cazhuo14@gmail.com>
2022-11-17 16:43:31 -0800

    Alert Controller app - a simple example

---
+++ AlertControllerExample/AlertControllerExample/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  AlertControllerExample
+//
+//  Created by Carmen Z on 2022/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var txtField: UITextField?
+    @IBOutlet weak var lblAlert: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Alert Controller Example", message: "Show Message", preferredStyle: .alert)
+        
+        let OKButton = UIAlertAction(title: "OK", style: .default) { action in
+            print("OK Button Pressed")
+            self.lblAlert.text = self.txtField?.text
+        }
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel) { action in
+            print("Cancel Button Pressed")
+        }
+        alertController.addAction(OKButton)
+        alertController.addAction(cancelButton)
+        
+        alertController.addTextField{ lblTextField in
+            self.txtField = lblTextField
+            lblTextField.placeholder = "Type something here"
+        }
+                
+        self.present(alertController, animated: true)
+    }
+    
+}
+
